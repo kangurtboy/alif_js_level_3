badef8ba76a18fbbb38898fcab5f2eafc114b3f3
o_ebrahim <kangurt.boy@gmail.com>
2023-08-01 19:29:49 +0500

    Выполнил дз 11 Alhamdulillah

---
+++ hw_11/main.js
@@ -0,0 +1,88 @@
+'use strict';
+const http = require('node:http');
+const posts = [
+  // { id: 0, content: 'bla', created: Date.now() },
+  // { id: 1, content: 'bla', created: Date.now() },
+  // { id: 2, content: 'bla', created: Date.now() },
+];
+const port = 9999;
+const status = {
+  //статусы ответа
+  bad: 400,
+  ok: 200,
+  noteFound: 404,
+};
+let dynamicId = 0;
+const path = '/posts';
+
+const sendResponse = {
+  //Отправка статусов
+  bad(response) {
+    response.writeHead(status.bad);
+    response.end();
+  },
+  notFound(response) {
+    response.writeHead(status.noteFound);
+    response.end();
+  },
+  ok(response) {
+    response.writeHead(status.ok, { 'Content-Type': 'application/json' });
+  },
+};
+
+const methods = new Map();
+
+methods.set(`${path}.post`, function (request, response) {
+  //Добавление поста
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const { searchParams } = url;
+  const content = searchParams.get('content');
+  if (!content) {
+    sendResponse.bad(response);
+    return;
+  }
+  const post = {
+    id: dynamicId++,
+    content: content,
+    created: Date.now(),
+  };
+  sendResponse.ok(response);
+  response.end(JSON.stringify(post));
+  posts.unshift(post);
+});
+
+methods.set(`${path}.get`, function (request, response) {
+  //Получение всех постов
+  sendResponse.ok(response);
+  response.end(JSON.stringify(posts));
+});
+
+methods.set(`${path}.getById`, function (request, response) {
+  //Получение поста по id
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const id = Number(url.searchParams.get('id'));
+  if (isNaN(id) || id === null) {
+    sendResponse.bad(response);
+    return;
+  }
+  const findedPost = posts.find((el) => el.id === id);
+  if (!findedPost) {
+    sendResponse.notFound(response);
+    return;
+  }
+  sendResponse.ok(response);
+  response.end(JSON.stringify(findedPost));
+});
+
+const server = new http.createServer((request, response) => {
+  //Сервер
+  const url = new URL(request.url, `http://${request.headers.host}`);
+
+  const method = methods.get(url.pathname);
+  if (!method) {
+    sendResponse.notFound(response);
+    return;
+  }
+  method(request, response);
+});
+server.listen(port);
